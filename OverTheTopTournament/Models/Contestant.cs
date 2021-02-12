e606a3331a0a11a8889ad9759dc40ef785abde99
jamesjr19 <ruwartja@gmail.com>
2021-02-11 22:11:57 -0500

    Set up Tournament and Contestant classes

---
+++ OverTheTopTournament/Models/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OverTheTopTournament.Models
+{
+    public class Contestant
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int NumberOfWins { get; set; }
+        public int NumberOfLosses { get; set; }
+
+    }
+}
---
+++ OverTheTopTournament/Models/Tournament.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OverTheTopTournament.Models
+{
+    public class Tournament
+    {
+        public DateTime DateOfTournament { get; set; }
+        public string Location { get; set; }
+        public int NumberOfContestants { get; set; }
+
+        //not sure if I need these here or in a different spot
+        public int FirstPlace { get; set; }
+        public int SecondPlace { get; set; }
+        public int ThirdPlace { get; set; }
+    }
+}
